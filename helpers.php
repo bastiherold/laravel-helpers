99aa104051463c6d465e5e2bc789adfe89b63318
Sebastian Herold <bastiherold@users.noreply.github.com>
2018-03-23 13:43:24 +0100

    Update helpers.php
    Merged areActiveRoute into isActiveRoute

--- helpers.php
+++ helpers.php
@@ -2,14 +2,15 @@
 
 function isActiveRoute($route, $output = 'active')
 {
-    return (Route::currentRouteName() == $route) ? $output : null;
-}
-
-function areActiveRoutes(Array $routes, $output = 'active')
-{
-    foreach ($routes as $route) {
-        if (Route::currentRouteName() == $route) return $output;
+    if (is_array($route)) {
+        foreach ($routes as $route) {
+            if (Route::currentRouteName() == $route) return $output;
+        }
+        
+        return null;
     }
+    
+    return (Route::currentRouteName() == $route) ? $output : null;
 }
 
 function containsActiveRoute($route , $output = 'active')
